41eb8ef8f86bacb27089c441f43cb7dee80a2e6b
Jimmy <jata2k@gmail.com>
2021-03-11 13:58:28 +0000

    Update vault path to cater for environment

--- groups/logging/locals.tf
+++ groups/logging/locals.tf
@@ -1,5 +1,5 @@
 data "vault_generic_secret" "secrets" {
-  path = "team-${var.team}/${var.account_name}/${var.region}/${var.repository_name}"
+  path = "team-${var.team}/${var.account_name}/${var.region}/${var.environment}/${var.repository_name}"
 }
 
 locals {

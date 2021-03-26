9a675edc8e8b2b2afd4c36d1e37b62ca25140f41
Jimmy <jata2k@gmail.com>
2021-03-26 15:29:48 +0000

    Simplify syntax for readability

--- groups/logging/locals.tf
+++ groups/logging/locals.tf
@@ -5,17 +5,17 @@ data "vault_generic_secret" "secrets" {
 locals {
   secrets = data.vault_generic_secret.secrets.data
 
-  concourse_worker_cidrs = jsondecode(local.secrets["concourse_worker_cidrs"])
+  concourse_worker_cidrs = jsondecode(local.secrets.concourse_worker_cidrs)
   discovery_availability_zones = join(",", list("${var.region}a", "${var.region}b", "${var.region}c"))
-  dns_zone_name = local.secrets["dns_zone_name"]
+  dns_zone_name = local.secrets.dns_zone_name
   data_cold_heap_size_gigabytes = var.instance_type_heap_allocation[var.data_cold_instance_type]
   data_hot_heap_size_gigabytes = var.instance_type_heap_allocation[var.data_hot_instance_type]
   data_warm_heap_size_gigabytes = var.instance_type_heap_allocation[var.data_warm_instance_type]
   internal_cidrs = values(data.terraform_remote_state.networking.outputs.internal_cidrs)
   placement_subnet_availability_zones = [for subnet in values(data.aws_subnet.placement) : lookup(subnet, "availability_zone")]
-  placement_subnet_name_patterns = jsondecode(local.secrets["placement_subnet_name_patterns"])
+  placement_subnet_name_patterns = jsondecode(local.secrets.placement_subnet_name_patterns)
   vpc_id = data.aws_vpc.vpc.id
-  vpc_name = local.secrets["vpc_name"]
+  vpc_name = local.secrets.vpc_name
   vpn_cidrs = values(data.terraform_remote_state.networking.outputs.vpn_cidrs)
 
   administration_cidrs = concat(local.internal_cidrs, local.vpn_cidrs)

a2e8efefba0490ad0994b5b119738094939d7534
Jimmy <jata2k@gmail.com>
2021-04-23 15:03:15 +0100

    Add local secrets variable for legibility

--- groups/prometheus/locals.tf
+++ groups/prometheus/locals.tf
@@ -3,13 +3,15 @@ data "vault_generic_secret" "secrets" {
 }
 
 locals {
+  secrets = data.vault_generic_secret.secrets.data
+
   discovery_availability_zones = join(",", list("${var.region}a", "${var.region}b", "${var.region}c"))
-  dns_zone_name = data.vault_generic_secret.secrets.data.dns_zone_name
+  dns_zone_name = local.secrets.dns_zone_name
   internal_cidrs = values(data.terraform_remote_state.networking.outputs.internal_cidrs)
   placement_subnet_availability_zones = [for subnet in values(data.aws_subnet.placement) : lookup(subnet, "availability_zone")]
-  placement_subnet_name_patterns = jsondecode(data.vault_generic_secret.secrets.data.placement_subnet_name_patterns)
+  placement_subnet_name_patterns = jsondecode(local.secrets.placement_subnet_name_patterns)
   vpc_id = data.aws_vpc.vpc.id
-  vpc_name = data.vault_generic_secret.secrets.data.vpc_name
+  vpc_name = local.secrets.vpc_name
   vpn_cidrs = values(data.terraform_remote_state.networking.outputs.vpn_cidrs)
 
   administration_cidrs = concat(local.internal_cidrs, local.vpn_cidrs)

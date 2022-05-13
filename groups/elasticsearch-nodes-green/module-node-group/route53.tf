fc99e27c7ab70a01c791cb2acbd13e15530bcdab
DrewHarvard <aharvard@companieshouse.gov.uk>
2022-05-13 12:32:12 +0100

    Update staging vars and fix route53 conditional config

--- groups/elasticsearch-nodes-green/module-node-group/route53.tf
+++ groups/elasticsearch-nodes-green/module-node-group/route53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "nodes" {
-  for_each = local.instance_definitions
+  for_each = var.route53_available ? local.instance_definitions : {}
 
   zone_id = data.aws_route53_zone.zone[0].zone_id
   name    = "${var.service}-${var.environment}-${var.deployment}-${var.group_name}-${each.key}.${var.dns_zone_name}"

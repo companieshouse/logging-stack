1ad060d41e275d1b64b1b947ffc00805eeac3853
Jimmy <jtaylor@companieshouse.gov.uk>
2024-04-05 08:52:06 +0100

    Fix provider role arn

--- groups/elasticsearch-alb/providers.tf
+++ groups/elasticsearch-alb/providers.tf
@@ -6,6 +6,6 @@ provider "aws" {
   alias   = "development_eu_west_1"
   region  = "eu-west-1"
   assume_role {
-    role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${data.aws_caller_identity.current.account_id}-terraform-lookup"
+    role_arn = "arn:aws:iam::${local.account_ids.development}:role/${data.aws_caller_identity.current.account_id}-terraform-lookup"
   }
 }
